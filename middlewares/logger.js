c359cf8eb8ffc7e09571b3b9a109e1022f17aeeb
aniahb <aniah6767@gmail.com>
2025-04-02 23:50:35 -0400

    fix: add console transport to errorLogger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -23,7 +23,10 @@ const requestLogger = expressWinston.logger({
 });
 
 const errorLogger = expressWinston.errorLogger({
-  transports: [new winston.transports.File({ filename: "error.log" })],
+  transports: [
+    new winston.transports.Console({ format: messageFormat }),
+    new winston.transports.File({ filename: "error.log" }),
+  ],
   format: winston.format.json(),
 });
 
